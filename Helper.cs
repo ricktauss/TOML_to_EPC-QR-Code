9172bf336630fe7bc4ab17e294b45b5a42e34386
Rick Tauss <tauss.rick@gmail.com>
2023-05-18 18:41:55 +0200

    Projektdateien hinzufügen.

---
+++ Helper.cs
@@ -0,0 +1,87 @@
+﻿using QRCoder;
+using SepaQr;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tomlyn;
+using Tomlyn.Model;
+using System.Text.RegularExpressions;
+
+namespace TomlToEpcQrCode
+{
+    public class Helper
+    {
+        public Dictionary<string, QRCode> tomlToEpcQrCode(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException();
+
+            string content = File.ReadAllText(filePath);
+            TomlTable model = Toml.ToModel(content);
+
+            //Get receiver information
+            TomlTable receiver = (TomlTable)model["receiver"];
+            string name = (string)receiver["name"];
+            string iban = (string)receiver["iban"];
+            string bic = (string)receiver["bic"];
+
+            if (String.IsNullOrEmpty(name) || String.IsNullOrEmpty(iban) || String.IsNullOrEmpty(bic))
+                throw new ArgumentException();
+
+            //Generate Receiver QR Code information
+            SepaQrCode qrCodeInfo = new SepaQrCode();
+            qrCodeInfo.SetName(name);
+            qrCodeInfo.SetAccountNumber(iban);
+            qrCodeInfo.SetBic(bic);
+
+            //Get payments 
+            Dictionary<string, QRCode> qrCodes = new Dictionary<string, QRCode>();
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+
+            TomlTableArray payments = (TomlTableArray)model["payments"];
+
+            foreach (TomlTable payment in payments)
+            {
+                //Generate payment info in qrCode
+                string reference = (string)payment["reference"];
+                decimal amount = (decimal)(double)payment["amount"];
+
+                qrCodeInfo.SetStructuredRemittanceInformation(reference);
+                qrCodeInfo.SetAmount(amount);
+
+                //Generate QR Code
+                string qrCodeContent = qrCodeInfo.GetQrContent();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrCodeContent, QRCodeGenerator.ECCLevel.M); // ECC level needs to be "M" per specification
+                QRCode qrCode = new QRCode(qrCodeData);
+
+                //Add to list
+                string qrCodeName = reference + "_" + amount;
+                qrCodes.Add(qrCodeName, qrCode);
+            }
+
+            return qrCodes;
+        }
+
+        public void saveQrCodesAsJpeg(Dictionary<string, QRCode> qrCodes, string folderPath)
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+                Console.WriteLine($"Folder created: {folderPath}");
+            }
+
+            foreach (KeyValuePair<string, QRCode> qrCode in qrCodes)
+            {
+                Bitmap qrCodeImage = qrCode.Value.GetGraphic(20);
+                string fileName = qrCode.Key + ".jpeg";
+                string filePath = folderPath + "\\" + fileName;
+                qrCodeImage.Save(filePath, ImageFormat.Jpeg);
+                Console.WriteLine($"QrCode created: {fileName}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using QRCoder;
+using TomlToEpcQrCode;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Helper helper = new Helper();
+
+        try
+        {
+            //Input information
+            string path = Directory.GetCurrentDirectory();
+            Console.WriteLine($"Searching for .toml files in {path} ...");
+            string[] files = Directory.GetFiles(path, "*.toml", SearchOption.AllDirectories);
+
+            if (files.Length <= 0)
+                throw new FileNotFoundException("Create TOML files in \"Payment\"-Folder according to template in repository or ReadMe-File");
+
+            Console.WriteLine($"{files.Length} Files found:");
+            Array.ForEach(files, file => Console.WriteLine(file));
+
+            Dictionary<string, QRCode> qrCodes = new Dictionary<string, QRCode>();
+    
+            foreach (string file in files)
+            {
+                qrCodes = helper.tomlToEpcQrCode(file);
+                helper.saveQrCodesAsJpeg(qrCodes, path + "\\QR-Codes");
+            }
+
+            Console.WriteLine("Program finished ... Press any key to continue");
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("\n" + ex + "\n");
+            Console.WriteLine("Program failed ... Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
